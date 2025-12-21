000e71d96626f0307d9325691646ab4226bc4db2
ritam-g <ritammaty2006@gmail.com>
2025-12-21 15:36:57 +0530

    adding sanke feture calcualting with 2D array

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ let boardColSize = Math.floor(board.clientWidth / blockWidth);
 // rows → based on HEIGHT
 let boardRowSize = Math.floor(board.clientHeight / blockHeight);
 
-console.log("Total blocks:", boardColSize * boardRowSize);
-for (let index = 0; index <boardColSize*boardRowSize; index++) {
-    let div=document.createElement("div")
-    div.classList.add("block")
-    board.appendChild(div)
+// console.log("Total blocks:", boardColSize * boardRowSize);
+// for (let index = 0; index <boardColSize*boardRowSize; index++) {
+//     let div=document.createElement("div")
+//     div.classList.add("block")
+//     board.appendChild(div)
+// }
+for (let row = 0; row < boardRowSize; row++) {
+
+    for (let col = 0; col < boardColSize; col++) {
+        let div=document.createElement("div")
+        div.classList.add("block")
+        div.innerHTML=`${row}-${col}`
+        board.appendChild(div)
+    }
+    
 }
